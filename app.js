37c97eba4d4329ca189315302c64410ea7595a9b
Sofie <solianyksofie@gmail.com>
2024-08-08 15:46:26 +0200

    Task 1

--- app.js
+++ app.js
@@ -22,12 +22,32 @@
 
 
 // ---------------------------------Task 2----------------------------------
-let firstNumber = prompt("Hi! Enter the first number, please!"), secondNumber = prompt("Great! Now enter the second number!");
 
-if(firstNumber > secondNumber)
+// let firstNumber = parseInt(prompt("Hi! Enter the first number, please!")), secondNumber = parseInt(prompt("Great! Now enter the second number!"));
+
+// if(firstNumber > secondNumber)
+// {
+//     document.write(firstNumber + " is greater than " + secondNumber);
+// }
+// else{
+//     document.write(secondNumber + " is greater than " + firstNumber);
+// }
+
+
+// ---------------------------------Task 3----------------------------------
+
+let number = prompt ("Hi! Enter a number which consists of 2 digits!");
+let firstDigit, lastDigit;
+
+lastDigit = number % 10;
+number -= lastDigit;
+number /=10;
+firstDigit = number;
+
+if(firstDigit > lastDigit)
 {
-    document.write(firstNumber + " is greater than " + secondNumber);
+    document.write("The first digit is greater than the last digit!");
 }
 else{
-    document.write(secondNumber + " is greater than " + firstNumber);
+    document.write("The last digit is greater than the first digit!");
 }
