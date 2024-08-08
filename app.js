3b797c46ba22bf2bc845deea96a76709bf6d6e29
Sofie <solianyksofie@gmail.com>
2024-08-08 15:45:14 +0200

    Task 1

---
+++ app.js
@@ -0,0 +1,33 @@
+// -----------------------------EXTRA PRACTICE------------------------------
+
+// ---------------------------------Task 1----------------------------------
+
+// let attempt = parseInt(prompt("Enter a number, please :)"));
+
+// switch(attempt)
+// {
+//     case 1:
+//         console.log("a");
+//         break;
+//     case 2:
+//         console.log("b");
+//         break;
+//     case 3:
+//         console.log("c");
+//         break;
+//     default:
+//        console.log("UA"); 
+//        break
+// }
+
+
+// ---------------------------------Task 2----------------------------------
+let firstNumber = prompt("Hi! Enter the first number, please!"), secondNumber = prompt("Great! Now enter the second number!");
+
+if(firstNumber > secondNumber)
+{
+    document.write(firstNumber + " is greater than " + secondNumber);
+}
+else{
+    document.write(secondNumber + " is greater than " + firstNumber);
+}
